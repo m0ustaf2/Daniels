80d2f3db974a91584481dd2259405c373732dad0
m0ustaf2 <mostafaesmail838@gmail.com>
2023-07-01 23:04:58 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// let homeContent=$('.home-content').offset().top;
+// console.log(homeContent);
+// $(window).scroll(function(){
+//     if($(window).scrollTop()>homeContent)
+//     {
+//         $('.navbar').css({'backgroundColor':'red','transation':'2s'})
+
+//     }
+//     else{
+//         $('.navbar').css('backgroundColor','transparent')
+//     }
+// })
